bcdd34fce6d4b130026efd9a2b29bfd0e200491f
Simon Metson <simon+github@cloudant.com>
2014-02-21 11:52:11 +0000

    add excellent

--- meme.py
+++ meme.py
@@ -11,6 +11,11 @@ class MemeOn(BotPlugin):
             "http://ruadouche.com/wp-content/uploads/2013/05/George-Takei-oh-my.jpg",
             "http://newnownext.mtvnimages.com/2013/11/george-takei-oh-my.png",
         ],
+        "excellent": [
+            "http://4.bp.blogspot.com/-w2dZIP16sew/TpE1L7g8IbI/AAAAAAAAAKA/kshUsHepP00/s1600/free-MrBurnsExcellent.gif",
+            "http://www.scancrit.com/wp-content/uploads/2011/11/excellent-4689_preview.png",
+            "http://static.rcgroups.net/forums/attachments/1/6/9/8/4/9/a2213358-88-mister-burns-excellent.jpg"
+        ]
     }
 
     def callback_message(self, conn, mess):
